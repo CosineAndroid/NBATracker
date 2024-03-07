c70570bec2ba47e5db630b98e9e41b28e9a59968
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-03-07 20:00:33 +0900

    feat: 컨퍼런스 모든 기준 추가

--- app/src/main/java/kr/cosine/nbatracker/enums/Conference.kt
+++ app/src/main/java/kr/cosine/nbatracker/enums/Conference.kt
@@ -3,6 +3,11 @@ package kr.cosine.nbatracker.enums
 enum class Conference(
     val koreanName: String
 ) {
+    ALL("모두"),
     EAST("동부"),
-    WEST("서부")
+    WEST("서부");
+
+    companion object {
+        val koreanNames = entries.map { it.koreanName }
+    }
 }
