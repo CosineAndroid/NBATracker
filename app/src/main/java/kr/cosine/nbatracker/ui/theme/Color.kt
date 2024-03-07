dc004782d275bf484000dde70a35b8270c3c3c61
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-03-07 20:03:14 +0900

    refactor: 최상위에서 object 클래스로 변경

--- app/src/main/java/kr/cosine/nbatracker/ui/theme/Color.kt
+++ app/src/main/java/kr/cosine/nbatracker/ui/theme/Color.kt
@@ -2,16 +2,22 @@ package kr.cosine.nbatracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Navy = Color(0xFF073042)
-val Blue = Color(0xFF4285F4)
-val LightBlue = Color(0xFFD7EFFE)
-val Chartreuse = Color(0xFFEFF7CF)
+object Color {
 
-// PlayerListActivity
-val TeamColor = Color(0xFF636363)
+    val Navy = Color(0xFF073042)
+    val Blue = Color(0xFF4285F4)
+    val LightBlue = Color(0xFFD7EFFE)
+    val Chartreuse = Color(0xFFEFF7CF)
 
-// PlayerActivity
-val PlayerScreenBackgroundColor = Color(0XFFC4C4C4)
-val PlayerImageBackgroundColor = Color(0xFF006BB6)
-val PlayerShortInfoBackgroundColor = Color(0xFF004270)
-val PlayerInfoGroupBackgroundColor = Color(0xFF005692)
+    val White = Color(0xFFFFFFFF)
+    val Gray = Color(0xFF888888)
+
+    // PlayerListActivity
+    val TeamColor = Color(0xFF636363)
+
+    // PlayerActivity
+    val PlayerScreenBackgroundColor = Color(0XFFC4C4C4)
+    val PlayerImageBackgroundColor = Color(0xFF006BB6)
+    val PlayerShortInfoBackgroundColor = Color(0xFF004270)
+    val PlayerInfoGroupBackgroundColor = Color(0xFF005692)
+}
