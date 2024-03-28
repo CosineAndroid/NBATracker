8483f48d6ae502eabb7aa15841fbf5fe406d5676
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-03-28 21:57:31 +0900

    feat: 팀에 선수 보이게 추가

--- app/src/main/java/kr/cosine/nbatracker/activity/PlayerListActivity.kt
+++ app/src/main/java/kr/cosine/nbatracker/activity/PlayerListActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,20 +15,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import kr.cosine.nbatracker.Text
 import kr.cosine.nbatracker.Head
+import kr.cosine.nbatracker.Space
 import kr.cosine.nbatracker.data.PlayerInfo
 import kr.cosine.nbatracker.model.PlayerInfoRegistry
 import kr.cosine.nbatracker.ui.theme.Color
+import kr.cosine.nbatracker.ui.theme.Font
 import kr.cosine.nbatracker.ui.theme.NBATrackerTheme
 
 class PlayerListActivity : ComponentActivity() {
@@ -51,25 +61,78 @@ class PlayerListActivity : ComponentActivity() {
 
 @Composable
 private fun Main(playerClickScope: (PlayerInfo) -> Unit) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Head("선수")
-        PlayerList(playerClickScope)
+        PlayerSearchBar()
+        PlayerCardList(playerClickScope)
     }
 }
 
 @Composable
-private fun PlayerList(playerClickScope: (PlayerInfo) -> Unit) {
+private fun PlayerSearchBar(viewModel: PlayerListViewModel = viewModel()) {
+    val searchInput = viewModel.searchInputStateFlow.collectAsState().value
+    BasicTextField(
+        value = searchInput,
+        onValueChange = viewModel::setSearchInput,
+        textStyle = TextStyle(
+            fontSize = 18.sp,
+            fontFamily = Font.esamanru,
+            fontWeight = FontWeight.Light
+        ),
+        singleLine = true,
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = Color.SearchBar,
+                        shape = RoundedCornerShape(8.dp)
+                    ).padding(4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "",
+                    tint = Color.SearchBarElement,
+                )
+                Space(width = 8.dp)
+                if (searchInput.isEmpty()) {
+                    Text(
+                        text = "검색",
+                        fontSize = 16.sp,
+                        color = Color.SearchBarElement
+                    )
+                } else {
+                    innerTextField()
+                }
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.07f)
+            .padding(7.dp)
+    )
+}
+
+@Composable
+private fun PlayerCardList(playerClickScope: (PlayerInfo) -> Unit, viewModel: PlayerListViewModel = viewModel()) {
+    val searchInput = viewModel.searchInputStateFlow.collectAsState().value
     LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color.White)
     ) {
-        itemsIndexed(PlayerInfoRegistry.getPlayerInfos()) { _, item ->
-            Player(item, playerClickScope)
+        itemsIndexed(PlayerInfoRegistry.getPlayerInfos(searchInput)) { _, item ->
+            PlayerCard(item, playerClickScope)
         }
     }
 }
 
 @Composable
-private fun Player(playerInfo: PlayerInfo, playerClickScope: (PlayerInfo) -> Unit) {
+private fun PlayerCard(playerInfo: PlayerInfo, playerClickScope: (PlayerInfo) -> Unit) {
     Card(
         colors = CardDefaults.cardColors(
             containerColor = Color.White
