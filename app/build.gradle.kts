fd28ed4b6b78ccfcfb40ea7d5180a0f563bee13e
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-06-09 20:53:35 +0900

    refactor: 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "kr.cosine.nbatracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
